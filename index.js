d9ed02099e0536e0bb48c1f22a868fecee8ab9c4
dhanushramudri <ramudridhanush@gmail.com>
2024-05-02 23:36:37 +0530

    created UserSchema

--- index.js
+++ index.js
@@ -13,21 +13,51 @@ app.get("/", (req, res) => {
   res.send("Backend is connected");
 });
 
-const DUMMY_DATA = {
-  name: "Dhanush",
-};
-
 const userShema = new mongoose.Schema({
-  name: String,
+  userId: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  fullName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    requried: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  primaryEmailAddress: {
+    imageUrl: String,
+    lastSignInAt: Date,
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  publicMetadata: {
+    type: Object,
+    default: {},
+  },
+  verifiedEmailAddress: {
+    type: Boolean,
+    default: false,
+  },
+  verifiedPhoneNumber: {
+    type: Boolean,
+    default: false,
+  },
 });
 const User = mongoose.model("User", userShema);
-try {
-  User.create(DUMMY_DATA).then((user) => {
-    console.log(user);
-    console.log(user.name);
-    console.log("user created");
-  });
-} catch (error) {}
 
 try {
   mongoose.connect(process.env.MONGODB_API).then(() => {
@@ -36,3 +66,5 @@ try {
 } catch (error) {
   console.log(error);
 }
+
+module.exports = User;
