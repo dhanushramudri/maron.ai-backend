9b528da3d46c3d28a889e8517c92d0ca713d0ac2
dhanushramudri <ramudridhanush@gmail.com>
2024-05-02 22:58:10 +0530

    database connected

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 const mongoose = require("mongoose");
 const app = express();
+require("dotenv").config();
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
@@ -11,3 +12,27 @@ app.listen(PORT, () => {
 app.get("/", (req, res) => {
   res.send("Backend is connected");
 });
+
+const DUMMY_DATA = {
+  name: "Dhanush",
+};
+
+const userShema = new mongoose.Schema({
+  name: String,
+});
+const User = mongoose.model("User", userShema);
+try {
+  User.create(DUMMY_DATA).then((user) => {
+    console.log(user);
+    console.log(user.name);
+    console.log("user created");
+  });
+} catch (error) {}
+
+try {
+  mongoose.connect(process.env.MONGODB_API).then(() => {
+    console.log("Connected to the database!");
+  });
+} catch (error) {
+  console.log(error);
+}
